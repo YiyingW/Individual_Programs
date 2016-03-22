808e76fe95d716cb038d7949df76903344f6a8ec
Yiying Wang <yiyingwang@yiyings-MacBook-Pro.local>
2016-03-22 14:54:54 -0400

    add first program. clarification needed.

---
+++ find_interval.py
@@ -0,0 +1,17 @@
+'''
+Functionality: 
+
+Input: 1) read in a file containing lines with format
+	   	  abc {[1:2]:10, [2:3]:20}
+	   	  ...
+
+	   	  note: there are at most 8 buckets.
+
+	   	2) name of the object, e.g. abc, 
+	   	   bucket number e.g.10
+
+Output: interval for that bucket, 1:2
+
+
+'''
+
